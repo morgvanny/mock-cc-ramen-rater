458b0c839d15338aaeeaabe4082b07853b201ece
Morgan VanYperen <morgvanny@gmail.com>
2022-07-27 15:39:19 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,23 +1,47 @@
 // write your code here
 const menu = document.querySelector("#ramen-menu");
 
+const displayRamenDetails = (ramen, image) => {
+  console.log(image);
+  const d = document.querySelector("#delete");
+  if (d) {
+    d.remove();
+  }
+  const detailImg = document.querySelector(".detail-image");
+  const detailName = document.querySelector(".name");
+  const detailRest = document.querySelector(".restaurant");
+  const detailRating = document.querySelector("#rating-display");
+  const detailComment = document.querySelector("#comment-display");
+  detailImg.src = ramen.image;
+  detailImg.alt = ramen.name;
+  detailName.innerText = ramen.name;
+  detailRest.innerText = ramen.restaurant;
+  detailRating.innerText = ramen.rating;
+  detailComment.innerText = ramen.comment;
+  const btn = document.createElement("btn");
+  btn.id = "delete";
+  btn.innerText = "Delete";
+  btn.addEventListener("click", () => {
+    detailImg.src = "";
+    detailImg.alt = "";
+    detailName.innerText = "";
+    detailRest.innerText = "";
+    detailRating.innerText = "";
+    detailComment.innerText = "";
+    btn.remove();
+    image.remove();
+  });
+  document.querySelector("#ramen-detail").append(document.createElement("br"));
+  document.querySelector("#ramen-detail").append(btn);
+};
+
 const addRamen = (ramen) => {
   const image = document.createElement("img");
   image.src = ramen.image;
   image.alt = ramen.name;
   menu.append(image);
-  image.addEventListener("click", (e) => {
-    const detailImg = document.querySelector(".detail-image");
-    const detailName = document.querySelector(".name");
-    const detailRest = document.querySelector(".restaurant");
-    const detailRating = document.querySelector("#rating-display");
-    const detailComment = document.querySelector("#comment-display");
-    detailImg.src = ramen.image;
-    detailImg.alt = ramen.name;
-    detailName.innerText = ramen.name;
-    detailRest.innerText = ramen.restaurant;
-    detailRating.innerText = ramen.rating;
-    detailComment.innerText = ramen.comment;
+  image.addEventListener("click", () => {
+    displayRamenDetails(ramen, image);
   });
 };
 
@@ -25,6 +49,7 @@ fetch("http://localhost:3000/ramens")
   .then((r) => r.json())
   .then((data) => {
     data.forEach(addRamen);
+    displayRamenDetails(data[0], menu.querySelector("img"));
   });
 
 document.querySelector("#new-ramen").addEventListener("submit", (e) => {
@@ -40,4 +65,13 @@ document.querySelector("#new-ramen").addEventListener("submit", (e) => {
   addRamen(ramen);
 
   e.target.reset();
+});
+
+document.querySelector("#edit-ramen").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const detailRating = document.querySelector("#rating-display");
+  const detailComment = document.querySelector("#comment-display");
+  detailRating.innerText = e.target.rating.value;
+  console.log(e.target["edit-comment"].value);
+  detailComment.innerText = e.target["edit-comment"].value;
 });
